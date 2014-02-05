27957fbee72b6716f7c848cdaa0658b22c2ca8c6
Alex Reinhart <alex@refsmmat.com>
2014-02-04 18:42:42 -0600

    remove whitespace and unneeded import

--- makeChains.py
+++ makeChains.py
@@ -4,14 +4,14 @@ Generate Markov chains to be cached in the filesystem; this saves us the effort
 of generating new chains every time we want to make poetry.
 """
 
-import sys, Markov, glob, os
+import sys, Markov, os
 
 def makeChain(name, sourceDir, cacheDir):
     """Parse the source text to generate new Markov chains."""
     sourceFile = sourceDir + name + ".txt"
-    
+
     fwd = Markov.MarkovChain(cacheDir + name + "-fwd", 1, True)
-    
+
     try:
         for line in open(sourceFile):
             words = line.split()
@@ -30,7 +30,6 @@ def deleteChainCache(name, cacheDir):
     except OSError:
         # if they don't exist, we don't care
         pass
-    
 
 if __name__ == "__main__":         
     if len(sys.argv) < 2:
@@ -41,8 +40,8 @@ where chainName is a specific source text to parse and cache."""
     
     print "Deleting old cache files"
     deleteChainCache(sys.argv[1], "cache/")
-    
+
     print "Creating new cache files"
     makeChain(sys.argv[1], "sources/", "cache/")
-    
+
     print "Chain(s) created successfully."
